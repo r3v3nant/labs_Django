99ff7856516d634ed745acccf49473acb3cf139a
r3v3nant <rv9nt.rv9nt@gmail.com>
2024-01-04 12:38:14 +0200

    lab5

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,9 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = OSp.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = OSp.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    OSp.join(BASE_DIR, 'static'),
+    OSp.join(BASE_DIR, 'staticfiles'),
 ]
 
 MEDIA_URL = '/media/'
